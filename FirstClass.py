16019949c5e228ea4404201181150d885d77429f
Akshay <akasurde@gmail.com>
2020-07-19 13:14:02 +0530

    Initial commit. Class added

---
+++ FirstClass.py
@@ -0,0 +1,9 @@
+# Create HOME blueprint and add music function
+class Home:
+    def music(self):
+        print("Rang De Basanti...!!!")
+
+
+# Create object from Home class
+my_home = Home()
+my_home.music()
